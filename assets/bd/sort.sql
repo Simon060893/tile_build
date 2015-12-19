87eb99664fd97a01ee0b054f067e5cf5e0028dc7
unilimes <unilimesdevs@gmail.com>
2015-12-19 22:43:12 +0200

    bd api

--- assets/bd/sort.sql
+++ assets/bd/sort.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 29, 2015 at 01:50 PM
+-- Generation Time: Dec 20, 2015 at 12:37 AM
 -- Server version: 5.5.25
 -- PHP Version: 5.3.13
 
@@ -29,10 +29,32 @@ SET time_zone = "+00:00";
 CREATE TABLE IF NOT EXISTS `products` (
   `id_product` int(11) NOT NULL AUTO_INCREMENT,
   `prdct_name` varchar(120) DEFAULT NULL,
+  `prdct_size` varchar(25) NOT NULL,
   `prdct_price` int(11) DEFAULT NULL,
   `id_sort` int(11) NOT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id_product`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=15 ;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id_product`, `prdct_name`, `prdct_size`, `prdct_price`, `id_sort`, `created`) VALUES
+(1, 'v1', '20*20', 4, 0, '2015-12-19 10:55:33'),
+(2, 'v2', '25*25', 7, 0, '2015-12-19 10:55:33'),
+(3, 'v3', '54*54', 31, 0, '2015-12-19 11:01:16'),
+(4, 'v4', '100*100', 105, 0, '2015-12-19 11:01:16'),
+(5, 'v5', '150*150', 236, 0, '2015-12-19 11:01:16'),
+(6, 'v6', '200*200', 420, 0, '2015-12-19 11:01:16'),
+(7, 'v7', '200*400', 656, 0, '2015-12-19 11:01:16'),
+(8, 'v8', '250*250', 179, 0, '2015-12-19 11:01:16'),
+(9, 'v9', '275*62', 116, 0, '2015-12-19 11:01:16'),
+(10, 'v10', '275*400', 945, 0, '2015-12-19 11:01:16'),
+(11, 'v11', '300*300', 1890, 0, '2015-12-19 11:01:16'),
+(12, 'v12', '300*600', 412, 0, '2015-12-19 11:01:16'),
+(13, 'v13', '400*98', 1618, 0, '2015-12-19 11:01:16'),
+(14, 'v16', '400*400', 105, 0, '2015-12-19 11:01:16');
 
 -- --------------------------------------------------------
 
@@ -52,6 +74,33 @@ CREATE TABLE IF NOT EXISTS `purchase` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `sorts`
+--
+
+CREATE TABLE IF NOT EXISTS `sorts` (
+  `id_sort` int(11) NOT NULL AUTO_INCREMENT,
+  `srt_name` varchar(150) NOT NULL,
+  `srt_adress` varchar(250) NOT NULL,
+  `srt_phone` varchar(10) NOT NULL,
+  `srt_email` varchar(50) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_sort`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+
+--
+-- Dumping data for table `sorts`
+--
+
+INSERT INTO `sorts` (`id_sort`, `srt_name`, `srt_adress`, `srt_phone`, `srt_email`, `id_user`, `created`) VALUES
+(0, 'sdf', 'dg', '0934415968', 'hgf@hj.ty', 0, '2015-12-13 10:18:41'),
+(2, 'sas', 'ds', '0934415968', 'as@f.cf', 0, '2015-12-13 11:36:12'),
+(3, 'sd', 'd', '0934415435', 'Ds@fd', 0, '2015-12-13 11:37:09'),
+(4, 'd', 'fd', 'r', 'df', 0, '2015-12-13 12:24:36');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -62,22 +111,21 @@ CREATE TABLE IF NOT EXISTS `users` (
   `usr_email` varchar(50) NOT NULL,
   `usr_phone` varchar(20) NOT NULL,
   `usr_ip` varchar(50) DEFAULT NULL,
-  `usr_date_crt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `usr_solt` varchar(20) NOT NULL,
+  `usr_type` varchar(20) NOT NULL,
   PRIMARY KEY (`id_user`),
   UNIQUE KEY `usr_email` (`usr_email`),
   UNIQUE KEY `usr_phone` (`usr_phone`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=40 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=50 ;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id_user`, `usr_name`, `usr_psw`, `usr_email`, `usr_phone`, `usr_ip`, `usr_date_crt`, `usr_solt`) VALUES
-(20, 'simom', '41e4jOQBgjGjk', 'pasku@bk.rh', '0934564212', NULL, '2015-11-21 10:35:45', '412'),
-(32, 'simom', '14Ol5C7zVA8wk', 'pasku@bk.ru', '0934564212k', NULL, '2015-11-21 10:43:04', '1448102583772'),
-(37, 'simom', '14T/ymf1.ww5.', 'pasku@bk.rur', '0934564254', NULL, '2015-11-21 10:50:26', '1448103025216'),
-(39, 'simom', '14axB.MN6zKt6', 'pdasku@bk.ru', '0544564212', NULL, '2015-11-21 10:55:34', '1448103333121');
+INSERT INTO `users` (`id_user`, `usr_name`, `usr_psw`, `usr_email`, `usr_phone`, `usr_ip`, `created`, `usr_solt`, `usr_type`) VALUES
+(47, 'senia', '14MuT1/YmTK6M', 'pasusam@gmail.com', '0936415968', NULL, '2015-12-12 14:14:06', '1449929645650', 'customer'),
+(49, 'senia', '14MuT1/YmTK6M', 'pasku@bk.ru', '0934415968', NULL, '2015-12-13 09:14:02', '1449998041576', 'executor');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
